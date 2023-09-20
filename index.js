2a3dea6a23768a53344e7dfbc036f42fc73d77a8
khoaletri <114121894+khoaletri@users.noreply.github.com>
2023-09-20 15:57:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on the port ${port}.`)
+})
